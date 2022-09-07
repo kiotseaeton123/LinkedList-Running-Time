50597b6bac2dbfebb8f01fa56c973bf56c525180
kiotseaeton lancaster <kiotseaeton@gmail.com>
2022-09-07 11:34:42 -0500

    did time complexity

--- LinkedList.h
+++ LinkedList.h
@@ -1,10 +1,12 @@
 // linkedList header file
+#include <iostream>
+
 struct Node
 {
 public:
     int key;
     Node *next;
-    Node * prev;
+    Node *prev;
 
     Node()
     { // default constructor
@@ -21,53 +23,99 @@ public:
 
 class LinkedList
 {
+public:
     Node *head;
+    Node *tail;
 
     LinkedList()
     {
-        head = nullptr;
+        head = tail = nullptr;
     }
     void _insertSorted(int _key);
-    void _delete(int _index);
+    void _delete(int _key);
     void _print();
 };
 
-void ::LinkedList::_insertSorted(int _key)
+void LinkedList::_insertSorted(int _key)
 {
-    Node *temp = new Node(_key);
+    Node *tempNew = new Node(_key);
 
     if (head == nullptr)
     {
-        head = temp;
+        head = tail = tempNew;
     }
     else
     {
         // insert new node at beginning if _key is smaller than head->key
         if (_key < head->key)
         {
-            temp->next = head;
-            head->prev = temp;
-            head = temp;
+            tempNew->next = head;
+            head->prev = tempNew;
+            tail = head;
+            head = tempNew;
+            return;
+        }
+        // insert new node at end of linkedList
+        if (_key > tail->key)
+        {
+            tail->next = tempNew;
+            tempNew->prev = tail;
+            tail = tempNew;
+            return;
         }
 
-        Node *_tempHead = head; //temp variable to traverse linkedList
+        Node *_tempHead = head; // temp variable to traverse linkedList
 
         // traverse linkedlist until the new node can be inserted into sorted position
-        while (_tempHead->next != nullptr && _key < _tempHead->next->key)
+        while (_tempHead->next != nullptr)
         {
             _tempHead = _tempHead->next;
+            
+            if (_tempHead->key > _key)
+            {
+                
+                break;
+            }
         }
-        
-        //insert new node into linkedList: link temp with previous node
+
+        // insert new node into linkedList: link temp with previous node
         Node *_tempPrev = _tempHead->prev;
-        _tempPrev->next = temp;
-        temp->prev = _tempPrev;
-        //insert new node into linkedList: link temp with next node
-        temp->next = _tempHead;
-        _tempHead->prev = temp;
+        _tempPrev->next = tempNew;
+        tempNew->prev = _tempPrev;
+        // insert new node into linkedList: link temp with next node
+        tempNew->next = _tempHead;
+        _tempHead->prev = tempNew;
     }
 }
 
-void::LinkedList::_delete(int _index){
-    
+void LinkedList::_delete(int _key)
+{
+    Node *_temp = head;
+
+    while (_temp->next != nullptr)
+    {
+        // if key is found, delete node and break out of loop
+        if (_temp->key == _key)
+        {
+            Node *_tempPrev = _temp->prev;
+            Node *_tempNext = _temp->next;
+            _tempPrev->next = _tempNext;
+            _tempNext->prev = _tempPrev;
+            break;
+        }
+        // key is not found, continue iterating thru linkedList
+        _temp = _temp->next;
+    }
+}
+
+void LinkedList::_print()
+{ // print check
+    Node *_temp = head;
+
+    while (_temp->next != nullptr)
+    {
+        std::cout << _temp->key << " ";
+        _temp = _temp->next;
+    }
+    std::cout << _temp->key << std::endl; // end string buffer
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,64 @@
 #include "LinkedList.h"
+#include <cstdlib> //rand() for random numbers
+#include <chrono>  //time functions
 
-int main(){
+void insertRandoms(int _number);
+
+int main()
+{
 
 	LinkedList myList;
+
+	// demonstrate functionality of insertSorted
 	myList._insertSorted(5);
+	myList._insertSorted(4);
+	myList._insertSorted(10);
+	myList._insertSorted(9);
+	myList._insertSorted(8);
+	myList._insertSorted(6);
+	myList._insertSorted(7);
+	std::cout << "Demonstrate functionality: ";
 	myList._print();
 
-	std::cout << "hello world" << std::endl;
+	// run for different values of "n"
+	insertRandoms(10000);
+	insertRandoms(20000);
+	insertRandoms(40000);
+	insertRandoms(80000);
+	insertRandoms(160000);
+	insertRandoms(320000);
+	insertRandoms(640000);
+	insertRandoms(1280000);
+
 	return 0;
 }
+
+void insertRandoms(int _number)
+{
+	LinkedList myList;
+
+	// chrono timing
+	using std::chrono::duration;
+	using std::chrono::duration_cast;
+	using std::chrono::high_resolution_clock;
+	using std::chrono::milliseconds;
+
+	std::cout << "n = " << _number << ": ";
+
+	auto _beginTime = high_resolution_clock::now(); // timer start
+
+	for (int i = 0; i < _number; i++)
+	{
+		int _randNum = rand() % _number;
+		myList._insertSorted(_randNum);
+	}
+
+	auto _endTime = high_resolution_clock::now(); // timer end
+
+	auto _timeTaken = duration_cast<milliseconds>(_endTime - _beginTime);
+
+	std::cout << _timeTaken.count() << std::endl; // end string buffer
+}
+
+// log1: inserting a largest element into sorted linkedList resulted in seg fault, due to this comparison when calling _insertSorted(): while(temp->next != nullptr && _key < temp->next->key)
+// resolution1: include tail pointer, and
